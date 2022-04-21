186942ac2d719b892a07b392cdbdd1433385e4c6
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2022-04-21 16:33:17 +0530

    Time: 42 ms (70.87%), Space: 27.5 MB (63.83%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,4 +1,33 @@
 class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int mid ;
+        while(start<=end)
+        {
+             mid = (start + end)/2;
+            if(nums[mid]==target)
+            {
+               
+                return mid;
+                
+            }
+            if(nums[mid]<target)
+            {
+                
+                start = mid+1;
+            }
+            else
+                end=mid-1;
+        }
+        return -1;
+        
+    }
+};
+
+
+/*class Solution {
   public:
   int search(vector<int>& nums, int target) {
     int pivot, left = 0, right = nums.size() - 1;
@@ -10,4 +39,4 @@ class Solution {
     }
     return -1;
   }
-};
+};*/
