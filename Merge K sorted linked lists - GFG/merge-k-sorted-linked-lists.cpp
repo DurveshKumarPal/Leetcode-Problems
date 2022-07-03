1e3bc26f905634922a94240b06d0cbeb132c7b17
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2022-07-03 23:19:14 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		printf("%d ", node->data);
+		node = node->next;
+	}
+	cout<<endl;
+}
+
+ // } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/ 
+
+class Solution{
+  public:
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K)
+    {
+        
+         priority_queue <int, vector <int>, greater <int> > pq;
+          Node *temp=new Node(-1);
+          Node *ans=new Node(-1);
+          Node *final=ans;
+          for(int i=0;i<K;i++){
+              temp->next=arr[i];
+              temp=temp->next;
+              while(temp->next!=NULL){
+                  pq.push(temp->data);
+                  temp=temp->next;
+              }
+              pq.push(temp->data);
+          }
+          while(!pq.empty()){
+              Node * temp=new Node(pq.top());
+              ans->next=temp;
+              ans=ans->next;
+              pq.pop();
+          }
+          return final->next;
+   }
+ 
+};
+
+
+
+// { Driver Code Starts.
+// Driver program to test above functions
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+	   int N;
+	   cin>>N;
+       struct Node *arr[N];
+       for(int j=0;j<N;j++)
+        {
+           int n;
+           cin>>n;
+
+           int x;
+           cin>>x;
+           arr[j]=new Node(x);
+           Node *curr = arr[j];
+           n--;
+
+           for(int i=0;i<n;i++)
+           {
+               cin>>x;
+               Node *temp = new Node(x);
+               curr->next =temp;
+               curr=temp;
+           }
+   		}
+   		Solution obj;
+   		Node *res = obj.mergeKLists(arr,N);
+		printList(res);
+
+   }
+
+	return 0;
+}
+  // } Driver Code Ends
