bdb4a9e27cabad4ebf91b1cc42d40c51960cd8cf
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2022-07-13 19:48:08 +0530

    Added solution - LeetHub

--- Queue Reversal - GFG/queue-reversal.py
+++ Queue Reversal - GFG/queue-reversal.py
@@ -2,24 +2,14 @@
 
 #Function to reverse the queue.
 def rev(q):
-    #using a stack to reverse the queue.
-    stack=[]
-    
-    #pushing elements from queue into stack and removing them from queue.
-    while(q.qsize()!=0):
-        x=q.get()
+    stack = []
+    while (q.qsize()!=0):
+        x =q.get()
         stack.append(x)
-        
-        
-    #now pushing elements back into the queue from stack and removing them 
-    #from stack. queue gets reversed as stack follows last in first out.
     while(len(stack)!=0):
-        x=stack.pop()
+        x =stack.pop()
         q.put(x)
-        
-    #returning reversed queue.
     return q
-
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
