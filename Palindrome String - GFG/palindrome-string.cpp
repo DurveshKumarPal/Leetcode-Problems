925d5ba0a5740a6a0027faf21ec3da85b08885cb
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2023-04-13 07:24:33 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -19,12 +19,12 @@ public:
 	   int ans=0;
 	   while(i<=j)
 	   {
-	       if(S[i]!=S[j])
+	       if(S[i++]!=S[j--])
 	       {
 	           return 0;
 	       }
-	       i++;
-	       j--;
+	       //i++;
+	       //j--;
 	      
 	   }
 	   return 1;
